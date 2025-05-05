7eceb85d993f4a27f2c5c25af79094a3277b2036
sonalivghotkule <165379385+sonalivghotkule@users.noreply.github.com>
2025-05-05 10:20:58 +0530

    SpringDocConfigProperties.java

---
+++ src/main/java/com/client/insights/config/SpringDocConfigProperties.java
@@ -0,0 +1,12 @@
+package com.client.insights.config;
+
+import org.springframework.context.annotation.Bean;
+
+public class SpringDocConfigProperties {
+
+
+    @Bean
+    public SpringDocConfigProperties springDocConfigProperties() {
+        return new SpringDocConfigProperties();
+    }
+}
